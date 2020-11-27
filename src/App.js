bbefcf201860c75ecf002e86c526363740b6bda0
Lukas Jury <Lukas.jury13@gmail.com>
2020-11-27 14:10:52 +1300

    fix some bugs

--- src/App.js
+++ src/App.js
@@ -10,14 +10,13 @@ function App() {
   const [image2, setImage2] = useState()
   const [loading, setLoading] = useState(false)
   const [score, setScore] = useState(0)
-  let url = "https://thesimpsonsquoteapi.glitch.me/quotes"
+  let url = "https://thesimpsonsquoteapi.glitch.me/quotes?count=10"
 
 
   let woohoo = new Audio("http://cs.klan-hub.ru/zombie/sound/misc/whoo.wav")
   let doh = new Audio("http://www.bobainsworth.com/wav/simpsons/doh.wav")
   const ranNum2 = Math.round(Math.random())
   function guesser(e) {
-    console.log(e.target.src)
     if (e.target.src === image) {
       setScore(prevScore => score + 1)
       handleClick()
@@ -33,43 +32,54 @@ function App() {
 
   const handleClick = () => {
     getQuote()
-    getQuote2()
   }
+
+
+
 
   useEffect(() => { getQuote() }, [])
   const getQuote = () => {
     setLoading(true)
     fetch(url).then(res => res.json())
       .then(data => {
-        setLoading(false)
-        const ranNum = Math.floor(Math.random() * Math.floor(data.length));
-        console.log(data[ranNum])
+
+        const ranNum = Math.floor(Math.random() * Math.floor(data.length -1));
+
+        function ranNum2() {
+          const ranNum2 = Math.floor(Math.random() * Math.floor(data.length -1));
+
+          if (ranNum === ranNum2 && ranNum2 !== data.length) {
+            return ranNum2 + 1
+          }
+          else if (ranNum === ranNum2 && ranNum2 === data.length) {
+            return ranNum2 - 1
+          }
+
+          else {
+            return ranNum2
+          }
+        }
+
+
+        const ranNum22 = ranNum2()
+        const ranNum3 = Math.round(Math.random())
+        console.log(ranNum22)
         setQuote(data[ranNum].quote)
         setCharacter(data[ranNum].character)
         setImage(data[ranNum].image)
-      }, [character, image, ranNum2])
-  }
-
-
-
-  useEffect(() => { getQuote2() }, [])
-  const getQuote2 = () => {
-    setLoading(true)
-    fetch(url).then(res => res.json())
-      .then(data => {
+        setCharacter2(data[ranNum22].character)
+        setImage2(data[ranNum22].image)
         setLoading(false)
-        const ranNum = Math.floor(Math.random() * Math.floor(data.length));
-        console.log(data[ranNum])
-        setCharacter2(data[ranNum].character)
-        setImage2(data[ranNum].image)
-      }, [character2, image2])
+      }, [character, character2, image, image2, ranNum2])
   }
-  console.log(ranNum2)
+
+
+
   return (
     <div className="generator">
       <h1>Score: {score}</h1>
       <h1 class="quote">"{quote}"</h1>
-      <div class="character-chooser" style={ranNum2 ? { flexDirection: "row-reverse" } : { flexDirection: "row" }}>
+      <div class="character-chooser" style={Math.round(Math.random()) === 1 ? { flexDirection: "row-reverse" } : { flexDirection: "row" } && !loading ? { display: "flex" } : { display: "none" }}>
         <div id="char1">
           <h2 class="character"> {character}</h2>
           <img onClick={(e) => guesser(e)} src={image}></img>
